a4490f1e4c8eb558c045bcd84e86873c202f39c7
Hong Zou <hong@turn.app>
2026-09-18 23:45:20 -0700

    Revert "Restore indexed indirect comments"
    This reverts commit dbc1831fea7a135a4c10e6835633b4d55ddb14ed.

--- MoltenVK/MoltenVK/Commands/MVKCmdDraw.mm
+++ MoltenVK/MoltenVK/Commands/MVKCmdDraw.mm
@@ -1409,9 +1409,9 @@ void MVKCmdDrawIndexedIndirect::encode(MVKCommandEncoder* cmdEncoder, const MVKI
             }
 
 			if (drawIdx == 0 || pipeline->isTessellationPipeline() || vtxAdjmts.needsAdjustment()) {
-				cmdEncoder->finalizeDrawState(stage);	// Ensure all updated state has been submitted to Metal
+				cmdEncoder->finalizeDrawState(stage);
 			}
-			if ( !pipeline->hasValidMTLPipelineStates() ) { return; }	// Abort if this pipeline stage could not be compiled.
+			if ( !pipeline->hasValidMTLPipelineStates() ) { return; }
 
             switch (stage) {
                 case kMVKGraphicsStageVertex:
